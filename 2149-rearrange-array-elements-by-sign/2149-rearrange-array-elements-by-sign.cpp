e9f1bade23f65a48f81761f82780ace39a69f0e7
Mohit Mittal <156795817+mohitmittal6989@users.noreply.github.com>
2026-07-13 04:19:16 +0530

    Time: 7 ms (55.68%), Space: 128.4 MB (54.28%) - LeetHub

--- 2149-rearrange-array-elements-by-sign/2149-rearrange-array-elements-by-sign.cpp
+++ 2149-rearrange-array-elements-by-sign/2149-rearrange-array-elements-by-sign.cpp
@@ -1,24 +1,41 @@
 class Solution {
 public:
     vector<int> rearrangeArray(vector<int>& nums) {
-        vector<int> pos;
-        vector<int> neg;
-        vector<int> sol;
+        // vector<int> pos;
+        // vector<int> neg;
+        // vector<int> sol;
 
-        for(auto k: nums){
-            if(k > 0) pos.push_back(k);
-            else neg.push_back(k);
+        // for(auto k: nums){
+        //     if(k > 0) pos.push_back(k);
+        //     else neg.push_back(k);
+        // }
+
+        // int i=0, j=0;
+
+        // while(i<pos.size() && j<neg.size()){
+        //     sol.push_back(pos[i]);
+        //     sol.push_back(neg[j]);
+        //     i++;
+        //     j++;
+        // }
+
+        // return sol;
+
+
+         vector<int> ans(nums.size());
+
+        int pos = 0, neg = 1;
+
+        for (int x : nums) {
+            if (x > 0) {
+                ans[pos] = x;
+                pos += 2;
+            } else {
+                ans[neg] = x;
+                neg += 2;
+            }
         }
 
-        int i=0, j=0;
-
-        while(i<pos.size() && j<neg.size()){
-            sol.push_back(pos[i]);
-            sol.push_back(neg[j]);
-            i++;
-            j++;
-        }
-
-        return sol;
+        return ans;
     }
 };
