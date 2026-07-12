8c18a1d617f9ae5a1e023e4b0ea2b1f04c0b2fa5
Mohit Mittal <156795817+mohitmittal6989@users.noreply.github.com>
2026-07-13 04:04:23 +0530

    Time: 2 ms (31.45%), Space: 97.4 MB (60.77%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.cpp
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.cpp
@@ -4,8 +4,12 @@ public:
         int buy = arr[0];
         int profit = 0;
         for(int i=1; i<arr.size(); i++){
-            if(arr[i] < buy) buy = arr[i];
-            else if(arr[i] > buy) profit = max(profit,arr[i]-buy);
+            // if(arr[i] < buy) buy = arr[i];
+
+            //else if(arr[i] > buy) profit = max(profit,arr[i]-buy);
+
+            profit = max(profit, arr[i]-buy);
+            buy = min(buy,arr[i]);
         }
 
         return profit;
