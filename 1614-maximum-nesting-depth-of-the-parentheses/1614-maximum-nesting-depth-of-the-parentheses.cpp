1093dae1d4b1c3bec2646169d9291f3fdd6710b5
Mohit Mittal <156795817+mohitmittal6989@users.noreply.github.com>
2026-07-20 00:01:24 +0530

    Time: 0 ms (100%), Space: 8.5 MB (24.02%) - LeetHub

--- 1614-maximum-nesting-depth-of-the-parentheses/1614-maximum-nesting-depth-of-the-parentheses.cpp
+++ 1614-maximum-nesting-depth-of-the-parentheses/1614-maximum-nesting-depth-of-the-parentheses.cpp
@@ -1,22 +1,35 @@
 class Solution {
 public:
     int maxDepth(string s) {
-        stack<char> st;
-        int ans = 0;
+        // stack<char> st;
+        // int ans = 0;
 
-        for(auto ch : s)
-        {
-            if(ch == '(')
-            {
-                st.push(ch);
-                ans = max(ans, (int)st.size());
+        // for(auto ch : s)
+        // {
+        //     if(ch == '(')
+        //     {
+        //         st.push(ch);
+        //         ans = max(ans, (int)st.size());
+        //     }
+        //     else if(ch == ')')
+        //     {
+        //         st.pop();
+        //     }
+        // }
+
+        // return ans;
+
+        int depth = 0;
+        int cnt = 0;
+
+        for(auto ch :s){
+            if(ch == '('){
+                depth++;
+                cnt = max(cnt, depth) ;
             }
-            else if(ch == ')')
-            {
-                st.pop();
-            }
+            else if(ch == ')') depth--;
         }
 
-        return ans;
+        return cnt;
     }
 };
