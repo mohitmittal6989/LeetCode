4b2e1364979eea552410bd0202903e6e4845ec78
Mohit Mittal <156795817+mohitmittal6989@users.noreply.github.com>
2026-07-19 23:48:31 +0530

    Time: 1 ms (68.57%), Space: 17.3 MB (5.77%) - LeetHub

--- 0451-sort-characters-by-frequency/0451-sort-characters-by-frequency.cpp
+++ 0451-sort-characters-by-frequency/0451-sort-characters-by-frequency.cpp
@@ -1,31 +1,53 @@
 class Solution {
 public:
     string frequencySort(string s) {
-        string ans = "";
-        map<char,int> m;
-        int n = s.size();
+        // string ans = "";
+        // map<char,int> m;
+        // int n = s.size();
 
-        for(auto ch:s){
-            m[ch]++;
+        // for(auto ch:s){
+        //     m[ch]++;
+        // }
+
+
+        // while(!m.empty()){
+        //     char maxChar;
+        //     int maxFreq = 0;
+
+        //     for(auto k:m){
+        //         if(k.second > maxFreq){
+        //             maxFreq = k.second;
+        //             maxChar = k.first;
+        //         }
+
+               
+        //     }
+        //     ans+=string(maxFreq,maxChar);
+        //     m.erase(maxChar);
+                    
+        
+        // }
+
+        // return ans;
+
+
+
+        map<char,int> m;
+        for(auto ch:s) m[ch]++;
+
+        vector<vector<char>> bucket(s.size()+1);
+
+
+        for(auto [ch,freq]: m){
+            bucket[freq].push_back(ch);
         }
 
 
-        while(!m.empty()){
-            char maxChar;
-            int maxFreq = 0;
+        string ans = "";
 
-            for(auto k:m){
-                if(k.second > maxFreq){
-                    maxFreq = k.second;
-                    maxChar = k.first;
-                }
-
-               
-            }
-            ans+=string(maxFreq,maxChar);
-            m.erase(maxChar);
-                    
-        
+        for(int i=s.size(); i>=1; i--){
+            for(auto ch:bucket[i]) ans+=string(i,ch);
+            
         }
 
         return ans;
