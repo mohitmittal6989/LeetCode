a227fa43b5b5a81b129618abb9642a8961035221
Mohit Mittal <156795817+mohitmittal6989@users.noreply.github.com>
2026-07-19 15:55:10 +0530

    Time: 0 ms (100%), Space: 8.8 MB (97.78%) - LeetHub

--- 1021-remove-outermost-parentheses/1021-remove-outermost-parentheses.cpp
+++ 1021-remove-outermost-parentheses/1021-remove-outermost-parentheses.cpp
@@ -1,36 +1,42 @@
 class Solution {
 public:
     string removeOuterParentheses(string s) {
-    //     string ans = "";
-    //     int open = 0;
-
-    //     for(char c : s){
-
-    //         if(c == '('){
-    //             if(open > 0)
-    //                 ans += c;
-
-    //             open++;
-    //         }
-    //         else{
-    //             open--;
-
-    //             if(open > 0)
-    //                 ans += c;
-    //         }
-    //     }
-
-    //     return ans;
         string ans = "";
-        stack<char> st;
+        int cnt = 0;
+
+        // for(char c : s){
+
+        //     if(c == '('){
+        //         if(open > 0)
+        //             ans += c;
+
+        //         open++;
+        //     }
+        //     else{
+        //         open--;
+
+        //         if(open > 0)
+        //             ans += c;
+        //     }
+        // }
 
         for(char ch:s){
-            if(ch == ')') st.pop();
-            if(!st.empty()) ans+=ch;
-            if(ch =='(') st.push(ch);
+            if(ch==')') cnt--;
+            if(cnt>0) ans+=ch;
+            if(ch=='(') cnt++;
         }
 
         return ans;
+        // string ans = "";
+        // stack<char> st;
+
+        // for(char ch:s){
+        //     if(ch == ')') st.pop();
+        //     if(!st.empty()) ans+=ch;
+        //     if(ch =='(') st.push(ch);
+        // }
+
+        // return ans;
 
     }
 
